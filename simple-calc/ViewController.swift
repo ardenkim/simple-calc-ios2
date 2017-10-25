e1446190d6b3fa965feb17a37474a38f7bb76ccf
Arden Kim <kmk3170@uw.edu>
2017-10-25 14:21:12 -0700

    Initial Commit

---
+++ simple-calc/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  simple-calc
+//
+//  Created by studentuser on 10/25/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
